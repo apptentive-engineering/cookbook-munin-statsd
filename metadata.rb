1fc4757613d024eb45f9ba87eafdcc7f9e15443b
Michael Saffitz <m@saffitz.com>
2014-12-19 12:59:17 -0800

    add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 maintainer       "Apptentive, Inc"
 maintainer_email "mike@apptentive.com"
 license          "BSD3"
+name             "munin-statsd"
 description      "Installs and Configures the munin-statsd.pl bridge"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
