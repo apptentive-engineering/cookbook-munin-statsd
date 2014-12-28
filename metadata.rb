3c5dbbec5e49c77f8c2038acfbf7df7a3fdb9d61
Michael Saffitz <m@saffitz.com>
2014-12-28 10:45:42 -0800

    use sys::hostname to get hostname, bump to 0.5

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ name             "munin-statsd"
 description      "Installs and Configures the munin-statsd.pl bridge"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          "0.4"
+version          "0.5"
 
 depends 'perl'
